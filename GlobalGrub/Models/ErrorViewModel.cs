bc85a9b017cb276b5c3f6876e0fb2221f65906b1
yndvp <58580623+yndvp@users.noreply.github.com>
2021-09-16 22:00:15 +0900

    Add project files.

---
+++ GlobalGrub/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlobalGrub.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
