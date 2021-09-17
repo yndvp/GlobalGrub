88d1ef3e75ebb94e5327f2acf2bfd0b8b335c565
yndvp <58580623+yndvp@users.noreply.github.com>
2021-09-17 20:47:34 +0900

    add category list with model

--- GlobalGrub/Controllers/CategoriesController.cs
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GlobalGrub.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,28 @@ namespace GlobalGrub.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            // use the Category model to create 10 categories objects in memory
+            var categories = new List<Category>();
+
+            for(var i = 1; i < 11; i++)
+            {
+                categories.Add(new Category() { CategoryId = i, Name = "Category " + i.ToString() });
+            }
+
+            // pass the categories list as strongly-typed datat to the view for display
+            return View(categories);
         }
 
         // categories/browse?category=abc - show selected category
         public IActionResult Browse(string category)
         {
+            // ensure we have a category value
+            if(category == null)
+            {
+                // redirect the user to Index so they can choose a category
+                return RedirectToAction("Index");
+            }
+
             // store the input parameter in a var inside the ViewBag so we can display the user's selection
             ViewBag.category = category;
 
