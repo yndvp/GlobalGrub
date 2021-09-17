9ee5302b870263cf95f4ec4509cd834d60488f38
yndvp <58580623+yndvp@users.noreply.github.com>
2021-09-17 17:59:43 +0900

    create category model

---
+++ GlobalGrub/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGrub.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // categories/browse?category=abc - show selected category
+        public IActionResult Browse(string category)
+        {
+            // store the input parameter in a var inside the ViewBag so we can display the user's selection
+            ViewBag.category = category;
+
+            return View();
+        }
+    }
+}
---
+++ GlobalGrub/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlobalGrub.Models
+{
+    public class Category
+    {
+        // all primary key fields in ASP.NET MVC should be called either {Model}Id or Id
+        // property names should always use PascalCase
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+    }
+}
