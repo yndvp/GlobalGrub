46f6c471052b239501b9e55c469db905fc161b44
yndvp <58580623+yndvp@users.noreply.github.com>
2021-11-14 21:26:30 -0500

    add social login option

--- GlobalGrub/Startup.cs
+++ GlobalGrub/Startup.cs
@@ -36,6 +36,17 @@ namespace GlobalGrub
                 .AddRoles<IdentityRole>() // now enable Role Management for Authorization - not on by default
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            // enable Google Auth
+            services.AddAuthentication().AddGoogle(options =>
+            {
+                // access Google Auth section of appsettings.json
+                IConfigurationSection googleAuth = Configuration.GetSection("Authentication:Google");
+
+                // read Google API Key values from config section and set as options
+                options.ClientId = googleAuth["ClientId"];
+                options.ClientSecret = googleAuth["ClientSecret"];
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
