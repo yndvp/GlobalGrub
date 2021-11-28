b090af783e6d695c4e13812be626f6e0a53cbd14
yndvp <58580623+yndvp@users.noreply.github.com>
2021-11-27 22:22:57 -0500

    create order history display

---
+++ GlobalGrub/Controllers/OrdersController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GlobalGrub.Data;
+using GlobalGrub.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace GlobalGrub.Controllers
+{
+    [Authorize]
+    public class OrdersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Orders
+        public async Task<IActionResult> Index()
+        {
+            if(User.IsInRole("Administrator"))
+            {
+                return View(await _context.Orders.ToListAsync());
+            }
+            else
+            {
+                var orders = await _context.Orders.Where(o => o.UserId == User.Identity.Name).ToListAsync();
+                return View(orders);
+            }
+            return View(await _context.Orders.ToListAsync());
+        }
+
+        // GET: Orders/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ThenInclude(oi => oi.Product)
+                .FirstOrDefaultAsync(m => m.OrderId == id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            // check user is Administrator or the owner of this order
+            if(!User.IsInRole("Administrator") && order.UserId != User.Identity.Name)
+            {
+                return RedirectToAction("Index");
+                
+            }
+
+            return View(order);
+        }
+
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderId == id);
+        }
+    }
+}
